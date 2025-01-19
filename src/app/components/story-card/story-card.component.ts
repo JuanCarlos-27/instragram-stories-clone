4896738bebe7bc8a4fce32c494a371cb50587fab
Juan Carlos <jcromero909@misena.edu.co>
2025-01-19 18:12:13 -0500

    Enhance story card component: add conditional rendering for image, implement navigation for adjacent stories, and improve data handling in TypeScript

--- src/app/components/story-card/story-card.component.ts
+++ src/app/components/story-card/story-card.component.ts
@@ -1,4 +1,4 @@
-import { NgStyle, NgTemplateOutlet, NgFor } from '@angular/common';
+import { NgStyle, NgTemplateOutlet } from '@angular/common';
 import {
   Component,
   computed,
@@ -6,7 +6,10 @@ import {
   signal,
   OnInit,
   OnDestroy,
+  inject,
+  effect,
 } from '@angular/core';
+import { Router } from '@angular/router';
 import { Reel, User } from '@app/interfaces/story.interface';
 
 @Component({
@@ -16,14 +19,24 @@ import { Reel, User } from '@app/interfaces/story.interface';
   styleUrl: './story-card.component.css',
 })
 export class StoryCardComponent implements OnInit, OnDestroy {
+  private router = inject(Router);
+
   public user = input.required<User>();
   public stories = input.required<Reel[]>();
+  public nextUserName = input.required<string>();
+  public prevUserName = input.required<string>();
 
   public storyPosition = signal(0);
   public progress = signal(0);
 
   public storyContent = computed(() => {
     return this.stories().at(this.storyPosition());
+  });
+
+  efecto = effect(() => {
+    console.log('NUevo efecto', this.user());
+    console.log('NUevo Story', this.stories());
+    console.log('NUevo storyPosition', this.storyPosition());
   });
 
   private timer: any;
@@ -33,6 +46,7 @@ export class StoryCardComponent implements OnInit, OnDestroy {
   }
 
   ngOnDestroy() {
+    console.log('Story card destroyed');
     clearInterval(this.timer);
   }
 
@@ -51,17 +65,25 @@ export class StoryCardComponent implements OnInit, OnDestroy {
   public nextStory = () => {
     this.storyPosition.update((position) => {
       const newPosition = position + 1;
-      if (newPosition >= this.stories().length) {
-        clearInterval(this.timer);
-        return position;
-      }
       return newPosition;
     });
+
+    if (this.storyPosition() === this.stories().length) {
+      this.router.navigate(['/stories', this.nextUserName()]);
+      this.storyPosition.set(0);
+    }
+
     this.progress.set(0);
   };
 
   public prevStory = () => {
     this.storyPosition.update((position) => Math.max(position - 1, 0));
+
+    if (this.storyPosition() === 0) {
+      this.router.navigate(['/stories', this.prevUserName()]);
+      this.storyPosition.set(0);
+    }
+
     this.progress.set(0);
   };
 }
